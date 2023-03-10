6fcab6017921ff4d4cef5f76b83bb3c09e5a3cc4
Ângelo Gabriel <angelogabriel207@outlook.com>
2023-03-10 16:05:51 -0300

    Criação: rota de salvar/atualizar products

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,6 +1,10 @@
 const {Router} =require('express')
+const productController = require('../controller/productController')
 
-const route = Router()
+const router = Router()
+
+// ===== Rotas de Produtos ======
+router.post('/products', productController.saveOrUpdate)
 
 
-module.exports = route
+module.exports = router
