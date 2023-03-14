d8dd7e603aae8dc4967364887b53401686c49b77
Ângelo Gabriel <angelogabriel207@outlook.com>
2023-03-14 11:27:40 -0300

    Criação: deletProduto

--- src/config/routes.js
+++ src/config/routes.js
@@ -8,6 +8,7 @@ router.post('/products', productController.saveOrUpdate)
 router.get('/products', productController.findAllProduct)
 router.put('/products', productController.saveOrUpdate)
 router.get('/products/:id', productController.findId)
+router.delete('/products/:id', productController.deleteProduct)
 
 
 module.exports = router
--- src/controller/productController.js
+++ src/controller/productController.js
@@ -3,7 +3,7 @@ const { Product } = require('../models/product')
 
 const saveOrUpdate = async (req, res) => {
     const product = { ...req.body }
-    if(req.params.id) product.id = req.params.id
+    if (req.params.id) product.id = req.params.id
 
     try {
         existsOrError(product.name, "[Err] Nome do produto não informado!")
@@ -17,8 +17,8 @@ const saveOrUpdate = async (req, res) => {
     }
 
     try {
-        
-        if(product._id) {
+
+        if (product._id) {
             const response = await Product.findById(product._id).exec()
 
             if (!response) {
@@ -39,8 +39,8 @@ const saveOrUpdate = async (req, res) => {
 
 const findAllProduct = async (req, res) => {
     const { page = 1, size = 5 } = req.query
-    
-    if(page <= 0 ) return res.status(400).json({message: "Página inválida!"})
+
+    if (page <= 0) return res.status(400).json({ message: "Página inválida!" })
 
     try {
         let skip = size * (page - 1)
@@ -48,22 +48,37 @@ const findAllProduct = async (req, res) => {
         res.status(200).json(list)
     } catch (error) {
         res.status(400).json(error)
-    }   
+    }
 }
 
 const findId = async (req, res) => {
-try {
+    try {
 
-    const response = await Product.findById(req.params.id).exec()
-    if(response) {
-        return res.status(200).json(response)
+        const response = await Product.findById(req.params.id).exec()
+        if (response) {
+            return res.status(200).json(response)
+        }
+
+        res.status(422).json({ message: "Produto não Encontrado" })
+    } catch (error) {
+        res.status(500).json(error)
     }
-
-    res.status(422).json({ message: "Produto não Encontrado" })
-} catch (error) {
-    res.status(400).json(error)
-}
-   
 }
 
-module.exports = { saveOrUpdate, findAllProduct, findId }
+const deleteProduct = async (req, res) => {
+    const id = req.params.id
+
+    try {
+        const responseDB = await Product.findOne({ _id: id })
+
+        if(!responseDB) return res.status(422).json({ message: "Produto não encontrado! " })
+
+        await Product.deleteOne({_id: id})
+        res.status(200).json({ message: "Produto Excluido!" })
+
+    } catch (error) {
+        res.status(500).json(error)
+    }
+}
+
+module.exports = { saveOrUpdate, findAllProduct, findId, deleteProduct }
